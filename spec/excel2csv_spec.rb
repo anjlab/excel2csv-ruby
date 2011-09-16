135e3ca0ff2e68790bfedad649b8bc2b132447c6
yury <yury.korolev@gmail.com>
2011-09-16 20:34:13 +0400

    Code cleanup a little bit

--- spec/excel2csv_spec.rb
+++ spec/excel2csv_spec.rb
@@ -5,16 +5,19 @@ require 'excel2csv'
 describe Excel2CSV do
 
   let(:excel) {Excel2CSV}
+  let(:csv_basic_types)  {"spec/fixtures/basic_types.csv"}
+  let(:xls_basic_types)  {"spec/fixtures/basic_types.xls"}
+  let(:xlsx_basic_types) {"spec/fixtures/basic_types.xlsx"}
 
   it "reads xls files" do
-    data = excel.read "spec/fixtures/basic_types.xls"
+    data = excel.read xls_basic_types
     data[0].should == ["1.00", "2011-12-23 21:00:00 UTC(+0000)", "Hello"]
     data[1].should == ["2.00", "2011-12-24 21:00:00 UTC(+0000)", "Привет"]
     data[2].should == ["3.00", "2011-12-25 21:00:00 UTC(+0000)", 'Привет, "я excel!"']
   end
 
   it "reads xlsx files" do
-    data = excel.read "spec/fixtures/basic_types.xlsx"
+    data = excel.read xlsx_basic_types
     data[0].should == ["1.00", "2011-12-23 21:00:00 UTC(+0000)", "Hello"]
     data[1].should == ["2.00", "2011-12-24 21:00:00 UTC(+0000)", "Привет"]
     data[2].should == ["3.00", "2011-12-25 21:00:00 UTC(+0000)", 'Привет, "я excel!"']
@@ -22,7 +25,7 @@ describe Excel2CSV do
 
   it "iterates rows" do
     count = 0
-    excel.foreach "spec/fixtures/basic_types.xls" do |row|
+    excel.foreach xls_basic_types do |row|
       row.length.should == 3
       count += 1
     end
@@ -31,7 +34,7 @@ describe Excel2CSV do
 
   it "removes tmp dir after work" do
     tmp_dir = nil
-    excel.convert "spec/fixtures/basic_types.xlsx" do |info|
+    excel.convert xlsx_basic_types do |info|
       # puts IO.read(info.sheets.first[:path])
       tmp_dir = info.tmp_dir
     end
@@ -40,20 +43,20 @@ describe Excel2CSV do
   end
 
   it "converts once if info is passed" do
-    info = excel.convert "spec/fixtures/basic_types.xlsx"
+    info = excel.convert xlsx_basic_types
     info.sheets.length.should == 1
     info.previews.length.should == 0
     info.should == excel.convert("spec/fixtures/basic_types.xlsx", info:info)
   end
 
   it "regenerate csv files if working_dir is removed" do
-    info = excel.convert "spec/fixtures/basic_types.xlsx"
+    info = excel.convert xlsx_basic_types
     info.clean
-    info.should_not == excel.convert("spec/fixtures/basic_types.xlsx", info:info)
+    info.should_not == excel.convert(xlsx_basic_types, info:info)
   end
 
   it "generates preview csv files with rows limit" do
-    info = excel.convert "spec/fixtures/basic_types.xls", rows_limit:1
+    info = excel.convert xls_basic_types, rows_limit:1
     info.sheets.length.should == 1
     info.previews.length.should == 1
 
@@ -65,20 +68,24 @@ describe Excel2CSV do
   end
 
   it "reads previews" do
-    data = excel.read("spec/fixtures/basic_types.xls", rows_limit:1, preview:true, index:0)
+    data = excel.read(xls_basic_types, rows_limit:1, preview:true, index:0)
     data.length.should == 1
     data[0].should == ["1.00", "2011-12-23 21:00:00 UTC(+0000)", "Hello"]
   end
 
   it "reads csv files" do
-    data = excel.read("spec/fixtures/basic_types.csv", encoding:'windows-1251:utf-8')
+    data = excel.read(csv_basic_types, encoding:'windows-1251:utf-8')
     data[0].should == ["1.00","12/24/11 12:00 AM","Hello"]
     data[1].should == ["2.00","12/25/11 12:00 AM","Привет"]
     data[2].should == ["3.00","12/26/11 12:00 AM",'Привет, "я excel!"']
   end
 
   it "reads csv files with preview" do
-    data = excel.read("spec/fixtures/basic_types.csv", encoding:'windows-1251:utf-8',rows_limit:2,preview:true)
+    data = excel.read(csv_basic_types,
+      encoding:   'windows-1251:utf-8',
+      rows_limit: 2,
+      preview:    true
+    )
     data[0].should == ["1.00","12/24/11 12:00 AM","Hello"]
     data[1].should == ["2.00","12/25/11 12:00 AM","Привет"]
   end
